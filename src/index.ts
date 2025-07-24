5ab182dd91013f955cffda1a0f35bbdcac20ddd4
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-07-24 17:30:30 +0530

    server setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+const PORT = Number(process.env.PORT) || 3000
+
+app.get("/", (req, res)=>{
+
+})
+
+//Listen to the server
+app.listen(PORT, ()=>{console.log(`Server is running on Port: ${PORT}`)})
