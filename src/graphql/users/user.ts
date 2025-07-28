04a32fb95cbaabe73452ff80dc9915753b844bb3
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-07-28 15:16:20 +0530

    user export

---
+++ src/graphql/users/user.ts
@@ -0,0 +1,6 @@
+import { mutations } from "./mutations"
+import { queries } from "./queries"
+import { resolvers } from "./resolvers"
+//import {typedefs} from "./typedef"
+
+export const User = {queries, mutations, resolvers}
